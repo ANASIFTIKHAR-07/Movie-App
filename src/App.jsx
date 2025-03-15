95330ba1aa51f2704b9e513006be1a2327dc0968
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-03-15 16:01:58 +0500

    Setting the project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern " />
+
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> That You'll Enjoy
+            Without Hassle
+          </h1>
+        </header>
+      </div>
+      <p>Search</p>
+    </main>
+  );
+};
+
+export default App;
