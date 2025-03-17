dfc57676372319f34c8f3084188ffe918c430c55
ANAS IFTIKHAR <131451392+ANASIFTIKHAR-07@users.noreply.github.com>
2025-03-17 14:24:56 +0500

    Api Fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,44 @@ import React, { useEffect, useState } from "react";
 import Search from "./components/Search.jsx";
 
 const API_BASE_URL = 'https://api.themoviedb.org/3' 
-const API_KEY = process.env.REACT_APP_TMDB_API_KEY
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY  
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useState('') 
+
+  const [errorMessage, setErrorMessage] = useState('')
+
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint =` ${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint, API_OPTIONS)
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+
+      const data = await response.json()
+      console.log(data) 
+      
+    } catch (error) {
+      console.error('Error fetching movies:', error)
+      setErrorMessage('An error occurred while fetching movies.')
+    } 
+  }
   
   useEffect(()=> {
-
-  }, [])
+    fetchMovies()
+  }, [])  
   return (
     <main>
       <div className="pattern " />
@@ -24,6 +53,11 @@ const App = () => {
           </h1>
               <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
+        <section className="all-movies">
+          <h2>All movies</h2> 
+
+          {errorMessage && <p className="error-message">{errorMessage}</p>} 
+        </section>
       </div>
     </main>
   );
